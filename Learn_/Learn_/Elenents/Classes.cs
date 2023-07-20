eff67d42cc4e3f60c9c3649e5f8a50b858a9f94d
00Artem00 <0.0.work.here.0.0@mail.ru>
2023-07-20 12:56:59 +0500

    Test

---
+++ Learn_/Learn_/Elenents/Classes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_
+{
+    class Tests
+    {
+    }
+}
---
+++ Learn_/Learn_/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        int digit_1 = 0;
+        int digit_2 = 0;
+        int digit_3 = 0;
+
+        static void Main()
+        {
+            int digit = Convert.ToInt32(Console.ReadLine());
+            Console.Write(digit);
+        }
+    }
+}
